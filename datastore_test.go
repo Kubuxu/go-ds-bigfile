46c6b620b720731e1d990df8be890c79d4694ac4
Jakub Sztandera <kubuxu@protonmail.ch>
2017-01-17 15:19:17 +0100

    Add more tests

--- datastore_test.go
+++ datastore_test.go
@@ -3,8 +3,17 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"path"
 	"testing"
 )
+
+const isSixtyFour = uint64(^uint(0)) == ^uint64(0)
+
+func TestIsSixtyFour(t *testing.T) {
+	if !isSixtyFour {
+		t.Fatal("expecting 64bit int")
+	}
+}
 
 func TestOpenEmptyDir(t *testing.T) {
 	dirName, err := ioutil.TempDir("", "bigfile")
@@ -23,14 +32,47 @@ func TestOpenEmptyDir(t *testing.T) {
 }
 
 func TestCreate(t *testing.T) {
+	size := int64(10 << 10)
 	dirName, err := ioutil.TempDir("", "bigfile")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(dirName)
 
-	err = Create(dirName, 10<<10)
+	err = Create(dirName, size)
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	fi, err := os.Stat(path.Join(dirName, BIGFILE_NAME))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if fi.Size() != size {
+		t.Fatalf("bad size, expected: %d, got %d", size, fi.Size())
+	}
+}
+
+func TestCreateOpen(t *testing.T) {
+	size := int64(10 << 10)
+	dirName, err := ioutil.TempDir("", "bigfile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dirName)
+
+	err = Create(dirName, size)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ds, err := Open(dirName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if int64(len(ds.mem)) != size {
+		t.Fatalf("bad size, expected: %d, got %d", size, len(ds.mem))
 	}
 }
